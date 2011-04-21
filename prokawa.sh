e5df265d2e249c49cc0d2689b06be85a8f8149ad
thomas <thomas@show.(none)>
2011-04-21 10:12:28 -0400

    initial commit to github

---
+++ prokawa.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export J=java
+export CLASSPATH=./target/prokawa-1.0-SNAPSHOT-jar-with-dependencies.jar
+export LIBPATH=/home/thomas/processing-1.5/modes/java/libraries/opengl/library/linux32/
+
+$J -cp $CLASSPATH -Djava.library.path=$LIBPATH kawa.repl $@
