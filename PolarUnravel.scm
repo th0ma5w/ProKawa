28490f1d51b450b17de5e485e26f9862a3a65e4e
Thomas <winningham@gmail.com>
2011-04-24 18:32:00 -0400

    cleanup

--- PolarUnravel.scm
+++ PolarUnravel.scm
@@ -4,14 +4,13 @@
 
 
 (define-simple-class PolarUnravel (processing.core.PApplet) 
-	(transition :: java.lang.Float init-value: (make java.lang.Float 0.0) )
-	(transTarget :: java.lang.Float init-value: (make java.lang.Float 0.0) )
-	(is :: toxi.math.SigmoidInterpolation init-value: (make toxi.math.SigmoidInterpolation 3))
+	(transition :: java.lang.Float init-value: (java.lang.Float 0.0) )
+	(transTarget :: java.lang.Float init-value: (java.lang.Float 0.0) )
+	(is :: toxi.math.SigmoidInterpolation init-value: (toxi.math.SigmoidInterpolation 3))
 	(col :: toxi.color.TColor )
 	(normUp :: toxi.geom.Vec2D )
 	((setup) 
 		(size screenWidth screenHeight codeanticode.glgraphics.GLConstants:GLGRAPHICS)
-		(smooth)
 	)
 	((draw)
 		(background 255) 
@@ -20,26 +19,27 @@
 		(translate ww hh)
 		(set! transition (+ transition (* (- transTarget transition ) 0.01) ) )
 		(set! col (toxi.color.TColor:newHSV 0 1 1))
-		(set! normUp (make toxi.geom.Vec2D 0 -1))
+		(set! normUp (toxi.geom.Vec2D 0 -1))
 		(for-each 	
-		  (lambda (i  :: gnu.math.IntNum )
-		   (let*  ((theta (radians i)))
-		    (let* ((polar (make toxi.geom.Vec2D 100 theta)))
-		     (col:setHue (modulo (/ (*:.y polar) TWO_PI) 1) )
-		      (let* ((len (* (noise (* (*:.y polar) 4)) 100 )))
-		       (let* ((circ ((polar:copy):toCartesian)))
-			(let* ((linear (make toxi.geom.Vec2D
-			 (+ (/ (* (- toxi.math.MathUtils:THREE_HALVES_PI
+		 (lambda (i  :: gnu.math.IntNum )
+		  (let*  ((theta (radians i)))
+		   (let* ((polar (toxi.geom.Vec2D 100 theta)))
+		    (col:setHue (modulo (/ (*:.y polar) TWO_PI) 1) )
+		     (let* ((len (* (noise (* (*:.y polar) 4)) 100 )))
+		      (let* ((circ ((polar:copy):toCartesian)))
+		       (let* ((linear (toxi.geom.Vec2D
+			(+ (/ (* (- 
+			 toxi.math.MathUtils:THREE_HALVES_PI
 			 (*:.y polar)
-			 ) ww ) PI ) ww)	
-			 0) ))
-			 (let* ((dir (circ:getNormalized)))
-			  (circ:interpolateToSelf linear transition is)
-			   ((dir:interpolateToSelf normUp transition is):normalizeTo len)
-			    (stroke (col:toARGB))
-			     (line (*:.x circ) (*:.y circ) (+ (*:.x circ) (*:.x dir)) (+ (*:.y circ) (*:.y dir)) )
+			) ww ) PI ) ww)	
+		       0) ))
+			(let* ((dir (circ:getNormalized)))
+			 (circ:interpolateToSelf linear transition is)
+			  ((dir:interpolateToSelf normUp transition is):normalizeTo len)
+			   (stroke (col:toARGB))
+			    (line (*:.x circ) (*:.y circ) (+ (*:.x circ) (*:.x dir)) (+ (*:.y circ) (*:.y dir)) )
 			 )))))))   
-			(iota 180 270 2) 
+		 (iota 180 270 2) 
 		)
 	   ))
 	)
@@ -49,6 +49,6 @@
 )
 
 (define p (PolarUnravel) )
-(PolarUnravel:runSketch (make String[] "--present" "PolarUnravel")  p)
+(PolarUnravel:runSketch (String[] "--present" "PolarUnravel")  p)
 
 
--- prokawa-toxi.sh
+++ prokawa-toxi.sh
@@ -2,6 +2,6 @@
 
 export J=java
 export CLASSPATH=./lib/toxiclibs/dist/colorutils.jar:./lib/toxiclibs/dist/toxiclibscore.jar:./target/prokawa-1.0-SNAPSHOT-jar-with-dependencies.jar:
-export LIBPATH=/home/thomas/processing-1.5/modes/java/libraries/opengl/library/linux32/
+export LIBPATH=./lib/toxiclibs/lib/jogl/
 
 $J -cp $CLASSPATH -Djava.library.path=$LIBPATH kawa.repl $@
